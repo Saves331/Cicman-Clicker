abcd2a27c46531435332fbfeab1910f1c96bb718
Martin Luterančík <martin.l74123@gmail.com>
2025-06-29 21:27:51 +0200

    added Protein Item

--- src/Items/Protein.jsx
+++ src/Items/Protein.jsx
@@ -1,9 +1,20 @@
-function Protein() {
+import { useState } from "react";
+import ProteinImg from "../images/Protein.webp"
+import count from "../count";
+
+function Protein(props) {
+  const {numberOfItem, price, name, onBuy} = props
+ 
+
+  function handleClick() {
+    onBuy();
+  }
+
   return (
     <div>
-
-    
-
+      <button onClick={handleClick}>Buy {name} for {price}</button>
+      <img src={ProteinImg} alt="" width={100} height={100}/>
+      <h2>{numberOfItem}</h2>
     </div>
   )
 }
--- src/count.jsx
+++ src/count.jsx
@@ -1,19 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ciciman from './images/cicmania.jpg'
+import Protein from "./Items/Protein"
 function count() {
 
     const [count, setCount] = useState(0)
+    const [numberOfItem, setNumberOfItem] = useState(0)
+    const [ProteinPrice, setProteinPrice] = useState(10)
+
+    const displayCount = count.toFixed(1)
+    const displayProteinPrice = ProteinPrice.toFixed(1)
 
     function handleClick() {
-        setCount(count+1)
+        setCount(prev => prev+1)
     }
+
+    function addProtein() {
+     
+      if(count >= ProteinPrice) {
+        setCount(prev => prev - ProteinPrice);
+        setNumberOfItem(prev => prev+1)
+        setProteinPrice(prevPrice => prevPrice*1.2)
+      }
+      
+    }
+
+    useEffect(() => {
+      let passiveProtein = numberOfItem * 0.1
+
+      const intervalID = setInterval(() => {
+         setCount(prev => prev + passiveProtein)
+      }, 1000)
+     
+      return () => clearInterval(intervalID)
+    }, [numberOfItem])
+
+    
 
   return (
     <div>
-        <h1>CicCount: {count}</h1>
+        <h1>CicCount: {displayCount}</h1>
         <button onClick={handleClick}><img src={ciciman} alt="" /></button>
 
-       
+        <Protein
+        name = "Protein"
+        price = {displayProteinPrice}
+        numberOfItem = {numberOfItem}
+        onBuy = {addProtein}
+
+        ></Protein>
     </div>
   )
 }
