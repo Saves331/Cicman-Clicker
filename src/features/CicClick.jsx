f15c9b7f421c04106d9f9e7c20c7778fd4ecca61
Saves <martin.l74123@gmail.com>
2025-10-03 20:39:11 +0200

    +1 on Click

--- src/features/CicClick.jsx
+++ src/features/CicClick.jsx
@@ -2,7 +2,30 @@ import React, { useState } from 'react'
 
 function CicClick(props) {
   const {ciciman, handleClick} = props;
-    const [clicked, setClicked] = useState(false)
+  const [clicked, setClicked] = useState(false)
+  const [plusOne, setPlusOne] = useState([])  
+
+    const handlePlusOne = (e) => {
+        const id = Date.now();
+
+        const rect = e.currentTarget.getBoundingClientRect();
+
+
+        const newPlus = {
+          id,
+          x: e.clientX - rect.left,
+          y: e.clientY - rect.top,
+        }
+
+      setPlusOne((prev) => [...prev, newPlus])
+
+      setTimeout(() => {
+        setPlusOne((prev) => prev.filter((p) => p.id !== id))
+      }, 1000)
+    }
+
+
+  
 
     const handleMouseDown = () => {
         setClicked(false);
@@ -14,19 +37,55 @@ function CicClick(props) {
             setClicked(false)
         }, 150) 
     }
+
+    const handleButtonClick = (e) => {
+        handleClick()
+        handlePlusOne(e)
+    }
   return (
+
+    <div className='relative inline-block'>
+        {plusOne.map((p) => (
+  <div
+    key={p.id}
+    className="absolute text-gray-600 font-bold animate-fade-up pointer-events-none z-11 text-2xl"
+    style={{ left: `${p.x - 10}px`, top: `${p.y - 30}px` }} // move it slightly above cursor
+  >
+    +1
+  </div>
+))}
+
     <button onMouseDown={handleMouseDown}
             onMouseUp = {handleMouseUp}
-            onClick={handleClick}
+            onClick={handleButtonClick}
             className={` cursor-pointer
          text-white rounded-full
-        transition-transform duration-300 ease-in-out active:scale-95
+        transition-transform duration-300 ease-in-out active:scale-95 z-10
         ${clicked ? "scale-105" : ""}
       `}
             
             >
         <img src={ciciman} className="h-100 rounded-full "/>
     </button>
+
+     <style jsx>{`
+        @keyframes fade-up {
+          0% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+          100% {
+            opacity: 0;
+            transform: translateY(-30px);
+          }
+        }
+        .animate-fade-up {
+          animation: fade-up 1s ease-out forwards;
+        }
+      `}</style>
+    </div>
+
+     
 
     
   )
